55f4131befb0523a3b6ddeb92f31576d5f5fc29a
Vladislav Nagorny <u.slash.vlad@gmail.com>
2022-01-19 19:36:27 +0300

    Problems 542,994

--- 994. Rotting Oranges/bfsearch.cpp
+++ 994. Rotting Oranges/bfsearch.cpp
@@ -1,27 +1,26 @@
 #include "../includes.hpp"
 using namespace std;
 
-// This solution goes from 0s to all nearest 1s, calculates distance and
-// stores it in the result matrix
-
 class Solution
 {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>> &matrix)
+    int orangesRotting(vector<vector<int>> &grid)
     {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> distances(m, vector<int>(n, INT32_MAX));
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> states(m, vector<int>(n, INT32_MIN));
         queue<pair<int, int>> coordsQueue;
-        
+
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if (matrix[i][j] == 0)
+                if (grid[i][j] == 1)
+                    states[i][j] = -1;
+                else if (grid[i][j] == 2)
                 {
-                    distances[i][j] = 0;
-                    coordsQueue.push({i, j}); // Put all 0s in the queue.
+                    states[i][j] = 0;
+                    coordsQueue.push({i, j});
                 }
             }
         }
@@ -38,14 +37,26 @@ public:
                 int newX = cell.second + dir[i][1];
                 if (newY >= 0 && newX >= 0 && newY < m && newX < n)
                 {
-                    if (distances[newY][newX] > distances[cell.first][cell.second] + 1)
+                    if (states[newY][newX] == -1)
                     {
-                        distances[newY][newX] = distances[cell.first][cell.second] + 1;
+                        states[newY][newX] = states[cell.first][cell.second] + 1;
                         coordsQueue.push({newY, newX});
                     }
                 }
             }
         }
-        return distances;
+
+        int result = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (states[i][j] == -1)
+                    return -1;
+                if (states[i][j] > result)
+                    result = states[i][j];
+            }
+        }
+        return result;
     }
 };
